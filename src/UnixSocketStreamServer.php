fc742e1d2098e1110ec79673bd2a65a06fec70dc
Vladimir Vrzić <vvrzic@gmail.com>
2021-11-23 23:21:51 +0100

    Improved checkMessages method
    - do not mark socket as nonblocking
    - recv with flags = 0
    - return on interrupted select
    - one accept per checkMessages call

--- src/UnixSocketStreamServer.php
+++ src/UnixSocketStreamServer.php
@@ -5,6 +5,7 @@ namespace TIPC;
 class UnixSocketStreamServer
 {
     const ESUCCESS = 0;
+    const EINTR = 4;
     const RECV_BUF_SIZE = 64 * 1024;
     const SOCKET_BACKLOG = 4 * 1024;
 
@@ -42,11 +43,8 @@ class UnixSocketStreamServer
     {
         $this->socket = socket_create(AF_UNIX, SOCK_STREAM, 0);
         if (($this->socket) === false) {
-            fwrite(
-                STDERR,
-                "socket_create() failed:" . PHP_EOL .
-                "reason: " . socket_strerror(socket_last_error()) . PHP_EOL
-            );
+            $error = socket_last_error($this->socket);
+            fwrite(STDERR, "socket_create() failed with error $error: " . socket_strerror($error) . PHP_EOL);
             return false;
         }
 
@@ -55,29 +53,14 @@ class UnixSocketStreamServer
         $bindSuccess = socket_bind($this->socket, $this->path);
         umask($umask);
         if ($bindSuccess === false) {
-            fwrite(
-                STDERR,
-                "socket_bind() failed:" . PHP_EOL .
-                "reason: " . socket_strerror(socket_last_error($this->socket)) . PHP_EOL
-            );
+            $error = socket_last_error($this->socket);
+            fwrite(STDERR, "socket_bind() failed with error $error: " . socket_strerror($error) . PHP_EOL);
             return false;
         }
 
         if (socket_listen($this->socket, self::SOCKET_BACKLOG) === false) {
-            fwrite(
-                STDERR,
-                "socket_listen() failed:" . PHP_EOL .
-                "reason: " . socket_strerror(socket_last_error($this->socket)) . PHP_EOL
-            );
-            return false;
-        }
-
-        if (socket_set_nonblock($this->socket) === false) {
-            fwrite(
-                STDERR,
-                "socket_set_nonblock() failed:" . PHP_EOL .
-                "reason: " . socket_strerror(socket_last_error($this->socket)) . PHP_EOL
-            );
+            $error = socket_last_error($this->socket);
+            fwrite(STDERR, "socket_listen() failed with error $error: " . socket_strerror($error) . PHP_EOL);
             return false;
         }
 
@@ -93,40 +76,38 @@ class UnixSocketStreamServer
     {
         $read = [$this->socket];
         $write = $except = null;
-        if (!(socket_select($read, $write, $except, $timeoutSeconds, $timeoutMicroseconds) > 0)) {
+        $num = @socket_select($read, $write, $except, $timeoutSeconds, $timeoutMicroseconds);
+        if ($num === false) {
+            $error = socket_last_error();
+            if ($error !== self::EINTR) {
+                fwrite(STDERR, "socket_select() failed with error $error: " . socket_strerror($error) . PHP_EOL);
+            }
             return;
         }
-        $limit = self::SOCKET_BACKLOG;
-        $cnt = 0;
-        while (($connectionSocket = socket_accept($this->socket)) !== false && ++$cnt < $limit) {
-            $msg = $this->receiveMessage($connectionSocket);
-            if ($msg !== null) {
-                $response = $this->msgHandler->handleMessage($msg);
-                $this->sendResponse($response, $connectionSocket);
+        if ($num > 0) {
+            foreach ($read as $socket) {
+                if (($connectionSocket = socket_accept($socket)) === false) {
+                    $error = socket_last_error($socket);
+                    if ($error !== self::ESUCCESS) {
+                        fwrite(STDERR, "socket_accept() failed with error $error: " . socket_strerror($error) . PHP_EOL);
+                        return;
+                    }
+                }
+                $msg = $this->receiveMessage($connectionSocket);
+                if ($msg !== null) {
+                    $response = $this->msgHandler->handleMessage($msg);
+                    $this->sendResponse($response, $connectionSocket);
+                }
             }
-        }
-        $error = socket_last_error($this->socket);
-        if ($error !== self::ESUCCESS) {
-            fwrite(
-                STDERR,
-                "socket_accept() failed:" . PHP_EOL .
-                "reason: " . socket_strerror($error) . PHP_EOL
-            );
-        }
-        if ($cnt > 0) {
-            fwrite(STDERR, "$cnt messages handled" . PHP_EOL);
         }
     }
 
     public function receiveMessage($connectionSocket)
     {
         fwrite(STDERR, '!' . PHP_EOL);
-        if (($bytes = socket_recv($connectionSocket, $buf, $this->recvBufSize, MSG_DONTWAIT)) === false) {
-            fwrite(
-                STDERR,
-                "socket_recv() failed" . PHP_EOL .
-                "Reason: ($bytes) " . socket_strerror(socket_last_error($this->socket)) . PHP_EOL
-            );
+        if (($bytes = socket_recv($connectionSocket, $buf, $this->recvBufSize, 0)) === false) {
+            $error = socket_last_error($this->socket);
+            fwrite(STDERR, "socket_recv() failed with error $error: " . socket_strerror($error) . PHP_EOL);
             return null;
         } else {
             fwrite(STDERR, "$bytes bytes received" . PHP_EOL);
