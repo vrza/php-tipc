394963791d5376935c66ce4173f8cdfeaf86156a
Vladimir Vrzić <vvrzic@gmail.com>
2022-03-23 09:52:24 +0100

    Use new SocketStreamClient

--- test/unix-socket-client.php
+++ test/unix-socket-client.php
@@ -5,7 +5,8 @@ error_reporting(E_ALL);
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use TIPC\UnixSocketStreamClient;
+use TIPC\SocketStreamClient;
+use TIPC\UnixDomainSocketAddress;
 
 const EXIT_USAGE = 64;
 const EXIT_NO_CONNECTION = 69;
@@ -18,7 +19,7 @@ if ($argc < 2) {
 }
 $msg = $argv[1];
 
-$client = new UnixSocketStreamClient($file);
+$client = new SocketStreamClient(new UnixDomainSocketAddress($file));
 if ($client->connect() === false) {
     exit(EXIT_NO_CONNECTION);
 }
